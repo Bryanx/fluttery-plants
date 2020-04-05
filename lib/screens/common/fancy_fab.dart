72bb148bdbebfcbe089a21c770ab4f97c0acf3b7
Bryan de Ridder <br.deridder@gmail.com>
2020-04-05 09:25:13 +0200

    Added labels to fab menu

--- lib/screens/common/fancy_fab.dart
+++ lib/screens/common/fancy_fab.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fluttering_plants/common/color_util.dart';
@@ -29,6 +30,7 @@ class _FancyFabState extends State<FancyFab>
   Animation<double> _animateIcon;
   Animation<double> _translateButton;
   Animation<double> _translateButtonX;
+  Animation<double> _fadeTween;
   double _fabHeight = 56.0;
 
   @override
@@ -39,52 +41,57 @@ class _FancyFabState extends State<FancyFab>
         setState(() {});
       });
     _animateIcon =
-        Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
-    _buttonColor = ColorTween(
-      begin: ColorUtil.primaryColor,
-      end: ColorUtil.lighten(ColorUtil.solidGrey, .1),
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(
-        0.00,
-        1.00,
-        curve: Curves.linear,
-      ),
-    ));
-    _buttonIconColor = ColorTween(
-      begin: ColorUtil.white,
-      end: ColorUtil.darken(ColorUtil.solidGrey, .1),
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(
-        0.00,
-        1.00,
-        curve: Curves.linear,
-      ),
-    ));
-    _translateButton = Tween<double>(
-      begin: _fabHeight,
-      end: -14.0,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(
-        0.0,
-        0.75,
-        curve: Curves.easeOut,
-      ),
-    ));
-    _translateButtonX = Tween<double>(
-      begin: _fabHeight,
-      end: -14.0,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Interval(
-        0.0,
-        0.75,
-        curve: Curves.easeOut,
-      ),
-    ));
+        Tween<double>(begin: 0.0, end: 0.75).animate(_animationController);
+    _buttonColor = getColorTween(ColorUtil.primaryColor, ColorUtil.lighten(ColorUtil.solidGrey, .1));
+    _buttonIconColor = getColorTween(ColorUtil.white, ColorUtil.darken(ColorUtil.solidGrey, .1));
+    _translateButton = getDoubleTween(_fabHeight, -14.0, 0.0, 0.5);
+    _translateButtonX = getDoubleTween(0.0, -65.0, 0.5, 0.75);
+    _fadeTween = getDoubleTween(0, 1.0, 0.5, 0.75);
     super.initState();
+  }
+
+  Animation<double> getFadeTween(double tweenBegin, double tweenEnd,
+      double intervalBegin, double intervalEnd) {
+    return Tween<double>(
+      begin: tweenBegin,
+      end: tweenEnd,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        intervalBegin,
+        intervalEnd,
+        curve: Curves.easeOut,
+      ),
+    ));
+  }
+
+  Animation<double> getDoubleTween(double tweenBegin, double tweenEnd,
+      double intervalBegin, double intervalEnd) {
+    return Tween<double>(
+      begin: tweenBegin,
+      end: tweenEnd,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        intervalBegin,
+        intervalEnd,
+        curve: Curves.easeOut,
+      ),
+    ));
+  }
+
+  Animation<Color> getColorTween(beginColor, endColor) {
+    return ColorTween(
+      begin: beginColor,
+      end: endColor,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(
+        0.00,
+        1.00,
+        curve: Curves.linear,
+      ),
+    ));
   }
 
   @override
@@ -100,6 +107,16 @@ class _FancyFabState extends State<FancyFab>
       _animationController.reverse();
     }
     isOpened = !isOpened;
+  }
+
+  Widget getFabText(String text) {
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 12.0),
+        decoration: BoxDecoration(
+            color: ColorUtil.white,
+            borderRadius: BorderRadius.all(
+                Radius.circular(24.0))),
+        child: Text(text));
   }
 
   Widget fertilize() {
@@ -140,6 +157,7 @@ class _FancyFabState extends State<FancyFab>
         focusElevation: 0.0,
         highlightElevation: 0.0,
         hoverElevation: 0.0,
+        splashColor: ColorUtil.lighten(openIconColor, .5),
         backgroundColor: openBgColor,
         foregroundColor: openIconColor,
         child: SvgPicture.asset("assets/icons/repot.svg", height: 25, color: ColorUtil.darken(openIconColor,.2)),
@@ -163,35 +181,30 @@ class _FancyFabState extends State<FancyFab>
     );
   }
 
+  getFabTransformText(double y, String text) => Transform(
+      transform: Matrix4.translationValues(_translateButtonX.value, y, 0.0),
+      child: FadeTransition(
+          opacity: _fadeTween,
+          child: getFabText(text)),
+    );
+
+  getFabTransform(double y, Widget child) => Transform(
+      transform: Matrix4.translationValues(0.0, y, 0.0),
+      child: child,
+    );
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
-        Transform(
-          transform: Matrix4.translationValues(
-            0.0,
-            _translateButton.value * 3.0,
-            0.0,
-          ),
-          child: fertilize(),
-        ),
-        Transform(
-          transform: Matrix4.translationValues(
-            0.0,
-            _translateButton.value * 2.0,
-            0.0,
-          ),
-          child: prune(),
-        ),
-        Transform(
-          transform: Matrix4.translationValues(
-            0.0,
-            _translateButton.value,
-            0.0,
-          ),
-          child: repot(),
-        ),
+        getFabTransformText(_fabHeight * 0.9, "Fertilize"),
+        getFabTransformText(_fabHeight * 1.7, "Prune"),
+        getFabTransformText(_fabHeight * 2.5, "Repot"),
+        getFabTransform(_translateButton.value * 3.0, fertilize()),
+        getFabTransform(_translateButton.value * 2.0, prune()),
+        getFabTransform(_translateButton.value, repot()),
         toggle(),
       ],
     );
